4507fc8d18dee628690c653c8744dde77bef386c
Konippi <konippi-114@outlook.jp>
2025-02-02 13:44:55 +0900

    chore: change to generate multiple topics

--- docker/kafka/create-topics.sh
+++ docker/kafka/create-topics.sh
@@ -1,9 +1,13 @@
-#!/bin/sh -eu
+#!/bin/bash -eu
 
-docker exec -it kafka /opt/bitnami/kafka/bin/kafka-topics.sh \
-      --bootstrap-server localhost:9094 \
-      --create \
-      --if-not-exists \
-      --topic chat \
-      --replication-factor 1 \
-      --partitions 1
+for TOPIC in $KAFKA_TOPICS; do
+    echo "Creating topic: $TOPIC"
+
+    /opt/bitnami/kafka/bin/kafka-topics.sh \
+        --bootstrap-server kafka:9092 \
+        --create \
+        --if-not-exists \
+        --topic "$TOPIC" \
+        --replication-factor 1 \
+        --partitions 1
+done
